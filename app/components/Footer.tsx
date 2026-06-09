d202757a98bf487a1c1a184ae4fae11b675857dc
un-canon <un-canon@hotmail.com>
2026-06-10 00:23:08 +0200

    页脚：移除左下角「西方負典.」巨型水印，仅保留右侧署名

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,10 +4,6 @@ export default function Footer() {
   return (
     <footer className="foot">
       <div className="foot-inner">
-        <div className="big">
-          {site.brandCN}
-          <span className="dots">.</span>
-        </div>
         <div className="meta">
           <b>{site.brand}</b>
           <br />
