54d6925e03aa8b03026a5921532b8e4aec269186
un-canon <un-canon@hotmail.com>
2026-07-12 01:54:49 +0200

    Add controlled mobile text justification

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import { getAllSlugs, getPostBySlug, getAdjacent } from "@/lib/posts";
 import { site } from "@/lib/site";
 import { RegisterArticleHeader } from "@/app/components/ArticleHeader";
+import MobileControlledJustify from "@/app/components/MobileControlledJustify";
 import ReadingRail from "@/app/components/ReadingRail";
 import TocRail from "@/app/components/TocRail";
 
@@ -100,6 +101,7 @@ export default async function ArticlePage({
         style={{ animationDelay: "0.06s" }}
         dangerouslySetInnerHTML={{ __html: post.html }}
       />
+      <MobileControlledJustify />
       <ReadingRail />
       <TocRail />
 
